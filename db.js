bc98d28ec29da3a5998eed6e4d861addf54e94ea
Vikram Sivota <introvikram@gmail.com>
2025-04-20 16:13:54 +0530

    latest changes

--- db.js
+++ db.js
@@ -1,5 +1,8 @@
 import mongoose from 'mongoose';
-const mongoURL = 'mongodb://localhost:27017/learning';
+import dotenv from 'dotenv';
+dotenv.config();
+//const mongoURL = 'mongodb://localhost:27017/learning';
+const mongoURL = process.env.DB_URL;
 
 mongoose.connect(mongoURL,{
     // useNewUrlParser: true,
--- server.js
+++ server.js
@@ -4,11 +4,13 @@ import db from './db.js';
 import Menu from './models/menu.js';
 import personRoutes from './routes/personRoutes.js';
 import menuRoutes from './routes/menuRoutes.js';
+import dotenv from 'dotenv';
+dotenv.config();
 const app = express();
 app.use(bodyParser.json());
 
 
 app.use('/person',personRoutes);
 app.use('/menu',menuRoutes);
-
-app.listen(3000 , () => { console.log('Server start....')});
+const PORT = process.env.PORT || 3000;
+app.listen(PORT , () => { console.log('Server start....')});
